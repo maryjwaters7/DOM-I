fe1a3d94187baab2cad88e2634f51d1a31ca3c1a
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-04-06 17:36:55 -0400

    completed all of adding content to page

--- js/index.js
+++ js/index.js
@@ -40,3 +40,87 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//NAV BAR
+let newNavArray = siteContent.nav;
+
+let navLinks = document.querySelectorAll('nav a');
+for (let i = 0; i < navLinks.length; i++) {
+  const contents = Object.values(newNavArray)
+  navLinks[i].textContent = contents[i];
+  navLinks[i].style.color = 'green';//ADD NEW CONTENT COLOR CHANGE
+}
+
+
+//HEADING SECTION
+const newCtaArray = siteContent.cta;
+const ctaArrayContent = Object.values(newCtaArray);
+
+const topH1 = document.querySelector('.cta h1')
+topH1.textContent = ctaArrayContent[0];
+
+const ctaButton = document.querySelector('.cta button');
+ctaButton.textContent = ctaArrayContent[1];
+
+const ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+
+//MAIN SECTION
+const newMainArray = siteContent["main-content"];
+const mainArrayContent = Object.values(newMainArray);
+
+const mainSections = document.getElementsByClassName('text-content');
+
+const features = mainSections[0];
+const featuresH4 = features.querySelector('h4');
+featuresH4.textContent = mainArrayContent[0];
+const featuresText = features.querySelector('p');
+featuresText.textContent = mainArrayContent[1];
+
+const about = mainSections[1];
+const aboutH4 = about.querySelector('h4');
+aboutH4.textContent = mainArrayContent[2];
+const aboutText = about.querySelector('p');
+aboutText.textContent = mainArrayContent[3];
+
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+const services = mainSections[2];
+const servicesH4 = services.querySelector('h4');
+servicesH4.textContent = mainArrayContent[5];
+const servicesText = services.querySelector('p');
+servicesText.textContent = mainArrayContent[6];
+
+const product = mainSections[3];
+const productH4 = product.querySelector('h4');
+productH4.textContent = mainArrayContent[7];
+const productText = product.querySelector('p');
+productText.textContent = mainArrayContent[8];
+
+const vision = mainSections[4];
+const visionH4 = vision.querySelector('h4');
+visionH4.textContent = mainArrayContent[9];
+const visionText = vision.querySelector('p');
+visionText.textContent = mainArrayContent[10];
+
+
+//CONTACT SECTION
+const newContactArray = siteContent["contact"];
+const contactArrayContent = Object.values(newContactArray);
+
+const contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = contactArrayContent[0];
+const contactP = document.querySelectorAll('.contact p');
+for (let i = 0; i < contactP.length; i++) {
+  contactP[i].textContent = contactArrayContent[i + 1]
+}
+
+
+//FOOTER SECTION
+const newFooterArray = siteContent['footer'];
+const footerArrayContent = Object.values(newFooterArray);
+
+const footerP = document.querySelector('footer p');
+footerP.textContent = footerArrayContent[0];
